47c24930699f26c19803889c1eb66905d19baefb
Yash <yashvdkd@gmail.com>
2022-10-22 14:25:18 +0530

    refactor: home

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,24 @@ module.exports = {
       animation: {
         marquee: 'marquee 7s linear infinite',
         marquee2: 'marquee2 7s linear infinite',
+        marqueeLg1: 'marqueeLg1 7s linear infinite',
+        marqueeLg2: 'marqueeLg2 7s linear infinite',
       },
       keyframes: {
         marquee: {
           '0%': { transform: 'translateX(0%)' },
-          '100%': { transform: 'translateX(-200%)' },
+          '100%': { transform: 'translateX(-300%)' },
         },
         marquee2: {
-          '0%': { transform: 'translateX(200%)' },
+          '0%': { transform: 'translateX(300%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+        marqueeLg1: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-105%)' },
+        },
+        marqueeLg2: {
+          '0%': { transform: 'translateX(105%)' },
           '100%': { transform: 'translateX(0%)' },
         },
       },
