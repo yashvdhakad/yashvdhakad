126a22b8e4a268339eea51b71b8a7c2dc27ede4c
Yash Dhakad <yashvdkd@gmail.com>
2023-06-13 03:29:25 +0530

    feat: focus!!! : about

--- src/components/main/About.jsx
+++ src/components/main/About.jsx
@@ -2,16 +2,9 @@ import React from 'react'
 
 const About = () => {
   return (
-    <article className='h-screen text-lg text-zinc-700 font-semibold space-y-4'>
-      <span className="block" >
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat error dignissimos repellendus maxime numquam tempore quidem illum cum optio aspernatur, dolore odio omnis ipsam corporis consequatur tempora possimus voluptas eligendi commodi qui, deleniti adipisci reprehenderit quae eveniet. Itaque asperiores eveniet, ex enim explicabo voluptate, incidunt rem soluta libero molestias, iusto excepturi nesciunt sint animi odit porro repellendus tenetur quisquam aspernatur molestiae earum ab voluptatem.
-      </span>
-      <span className="block" >
-        Facere corrupti quasi iusto id, harum, dignissimos deserunt vel commodi assumenda, aperiam veritatis et dolor provident dolorem velit adipisci asperiores voluptatibus earum! Iste non consectetur porro consequatur perspiciatis aliquam, quidem suscipit provident quae ut nam sed repellendus? Ut quidem libero eos, enim amet exercitationem recusandae, nobis incidunt neque magnam ad expedita maiores suscipit fugit consequuntur! Unde vero ad adipisci enim soluta natus nobis inventore explicabo, fugit, hic veniam?</span>
-      <span className="block" >
-        Dolor adipisci labore commodi ea repellat ab facere consequuntur, necessitatibus accusantium corporis fugiat obcaecati dolorum? Dolor nisi rem neque ducimus quam quas, placeat recusandae deleniti excepturi est molestias tenetur aliquam minus praesentium sed debitis nulla officiis explicabo mollitia beatae eius fuga eaque! Assumenda, quam nesciunt nam natus blanditiis officiis culpa eos quod iste ullam dolorem minus ipsa quisquam fugiat. Amet, maxime quidem recusandae deserunt minima animi doloremque, similique tenetur quod alias perspiciatis assumenda pariatur atque? Fugit ex sapiente hic esse natus dignissimos beatae! Veritatis quo dicta soluta ex quia consequuntur nobis sint dolorem non molestiae hic, ullam mollitia tenetur nostrum quidem eos fugiat odio dolor sunt quos quis accusantium architecto laudantium cumque.
-      </span>
-    </article>
+    <section className='h-screen py-10 text-zinc-800 font-bold space-y-4'>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque soluta recusandae nesciunt voluptate, ea ad repudiandae tempore.
+    </section>
   )
 }
 
