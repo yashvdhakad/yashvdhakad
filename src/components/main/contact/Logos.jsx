a17fad2c940521479b74a0d27e0bc2590d42a44d
Yash Dhakad <yashvdkd@gmail.com>
2023-06-19 03:11:43 +0530

    refactor: bootstrap icons added

--- src/components/main/contact/Logos.jsx
+++ src/components/main/contact/Logos.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
-import Github from '../../../assets/img/github.png'
-import Twitter from '../../../assets/img/twitter.png'
-import Instagram from '../../../assets/img/instagram.png'
-import LinkedIn from '../../../assets/img/linkedin.png'
 
 const Logos = () => {
     return (
         <div className="flex flex-row justify-center items-center space-x-10 opacity-50">
-            <a target='blank' href="https://github.com/yashvdhakad"><img width="40" src={Github}></img></a>
-            <a target='blank' href="https://twitter.com/yashvdhakad"><img width="40" src={Twitter}></img></a>
-            <a target='blank' href="https://instagram.com/yashvdhakad"><img width="40" src={Instagram}></img></a>
-            <a target='blank' href="https://linkedin.com/in/yashvdhakad"><img width="40" src={LinkedIn}></img></a>
+            <a target='blank' href="https://github.com/yashvdhakad"><i className="bi bi-github" style={{fontSize: "2.5rem", color: "black"}}></i></a>
+            <a target='blank' href="https://twitter.com/yashvdhakad"><i className="bi bi-twitter" style={{fontSize: "2.5rem", color: "black"}}></i></a>
+            <a target='blank' href="https://instagram.com/yashvdhakad"><i className="bi bi-instagram" style={{fontSize: "2.5rem", color: "black"}}></i></a>
+            <a target='blank' href="https://linkedin.com/in/yashvdhakad"><i className="bi bi-linkedin" style={{fontSize: "2.5rem", color: "black"}}></i></a>
         </div>
     )
 }
