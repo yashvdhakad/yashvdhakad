980f82c46ef549f2e43725a78cc656914fe65308
Yash Dhakad <yashvdkd@gmail.com>
2023-07-28 11:31:39 +0530

    refactoring

---
+++ yashvdhakad/app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className='relative max-w-screen-lg m-auto flex justify-center items-center'>
+      <main>
+        Main
+      </main>
+    </div>
+  )
+}
