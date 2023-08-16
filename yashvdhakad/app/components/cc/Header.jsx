e6ab82ecc3feb82d7b503b8386de8b8a410127a0
Yash Dhakad <yashvdkd@gmail.com>
2023-08-17 01:09:55 +0530

    fonts added

--- yashvdhakad/app/components/cc/Header.jsx
+++ yashvdhakad/app/components/cc/Header.jsx
@@ -12,12 +12,12 @@ const Header = () => {
       ]
 
     return (
-        <nav className='fixed bottom-10 w-fit rounded-xl p-2 flex space-x-2 bg-neutral-800 shadow-xl shadow-zinc-800/40 z-50'>
-            <div className='p-2 rounded-lg bg-neutral-700 flex flex-row flex-wrap j
+        <nav className='fixed bottom-10 w-fit rounded-xl p-2 flex space-x-2 bg-zinc-800 shadow-xl shadow-zinc-800/40 z-50 font-[Akira-Bold] tracking-widest'>
+            <div className='p-2 rounded-lg bg-zinc-700 flex flex-row flex-wrap j
             ustify-center items-center gap-2'>
                 {
                     navArr.map((i) => {
-                        return <Link key={i.name} className="border border-[#333239] bg-neutral-800 text-zinc-100 rounded-lg px-4 py-2 transition-all cursor-pointer shadow-sm shadow-neutral-800/60 hover:scale-95 hover:bg-neutral-900 hover:border-[#1c1b22] focus:bg-zinc-900" href={i.url} >{i.name==="/" ? <Image className='invert opacity-80' src={homeIcon} width={24} height={24} /> : i.name}</Link>
+                        return <Link key={i.name} className="border border-[#333239] bg-zinc-800 text-zinc-100 rounded-lg px-4 py-2 transition-all cursor-pointer shadow-sm shadow-zinc-800/60 hover:scale-95 hover:bg-zinc-900 hover:border-[#1c1b22] focus:bg-zinc-900" href={i.url} >{i.name==="/" ? <Image className='invert opacity-80' src={homeIcon} width={24} height={24} /> : i.name}</Link>
                     })
                 }
             </div>
