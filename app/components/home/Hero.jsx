85b2f1a61148e7ff094b275ba5fa2a006354cc00
Yash Dhakad <yashvdkd@gmail.com>
2023-10-21 14:51:27 +0530

    refactor: :fire: rebuilding...

--- app/components/home/Hero.jsx
+++ app/components/home/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
   return (
     <section className="w-full mt-36 mb-60 flex flex-row lg:flex-nowrap flex-wrap justify-between items-center gap-6 select-none">
       {/* dp */}
-      <div className="relative -scale-x-100">
+      {/* <div className="relative -scale-x-100">
         <Image
           className="w-full border border-white/10 rounded-lg shadow-lg"
           src={dp}
@@ -33,7 +33,7 @@ const Hero = () => {
           alt="Yash Dhakad DP"
           height={dpSize}
         />
-      </div>
+      </div> */}
       {/* text and marquee */}
       {/* <div className="w-8/12 flex flex-col gap-6">
         <div className="p-6 flex flex-col items-center gap-6 text-center border border-white/10 rounded-lg drop-shadow shadow bg-main bg-cover">
@@ -46,8 +46,8 @@ const Hero = () => {
         </div>
         <LogoMarquee speed={40} />
       </div> */}
-      <h1 className="text-[240px] font-bold tracking-tighter uppercase">
-        10x ROI
+      <h1 className="text-9xl font-bold tracking-tighter uppercase">
+        Building...
       </h1>
     </section>
   );
